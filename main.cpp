2e290a059c202df9e2c324f6b91c624b92e6196f
Tanmayee Chalamalasetti <tanmayeesc@Tanmayees-MacBook-Pro-190.local>
2025-10-26 20:29:21 -0700

    writing race 1

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
-#include <iostream>
+// COMSC210 | Lab 25 | Tanmayee Chalamalasetti
+// IDE Used: VS Code
 #include <chrono>
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <set>
+#include <string>
+#include <vector>
 using namespace std;
+using namespace std::chrono;
 
 int main() {
-    
+  string file = "codes.txt";
+  string s;
+  // Race 1: Reading
 
-    return 0;
+  // vector
+  auto start = high_resolution_clock::now();
+  ifsteam fin1(file);
+  vector<string> v;
+  while (fin1 >> s) {
+  }
+
+  return 0;
 }
 
 /* syntax examples:
